94d512843d26b4656acf3078c8489662a08952fd
Simon Carr <simonjcarr@gmail.com>
2023-03-04 12:41:17 +0000

    test: add tests for counter
    * test: add tests for counter
    
    * test: fix test variable name
    
    ---------
    
    Co-authored-by: Simon <simon@soxprox.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h3>Counter</h3>
-    <button @click="counter--">-</button>
+    <button id="decrement" @click="counter--">-</button>
     <input type="text" v-model="counter">
-    <button @click="counter++">+</button>
+    <button id="increment" @click="counter++">+</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
